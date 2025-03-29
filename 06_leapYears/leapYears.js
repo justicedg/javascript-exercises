dfe2abf8d471f717cfef7ccdaccd9e877bcff1e4
justicedg <justicedgutierrez@gmail.com>
2025-03-29 14:48:39 -0500

    Complete javascript exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    const isYearDivisibleBy4 = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleBy400 = year % 400 === 0;
 
+    if (
+        isYearDivisibleBy4 &&
+        (!isCentury || isYearDivisibleBy400)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
